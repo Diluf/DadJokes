0851093ca5890b78fee2336e17edde874d25e9af
Dilup <dilup.thivanka@vantage-tag.com>
2021-12-23 11:24:54 -0800

    was

--- app/src/main/java/com/debugsire/dadjokes/MainActivity.java
+++ app/src/main/java/com/debugsire/dadjokes/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
 
 
 
